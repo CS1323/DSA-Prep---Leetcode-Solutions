2ed26bb5b79a2b89eede32682981cbb814f14219
Cheyenne Sterbick <sterbickc@gmail.com>
2025-08-06 22:16:51 -0700

    feat: add Leetcode 209 solution
    Minimum Size Subarray Sum
    Practice for arrays and sliding window.

---
+++ 209_MinimumSizeSubarraySum.py
@@ -0,0 +1,24 @@
+def minSubArrayLen(target, nums) -> int:
+    nums_length = len(nums)
+    left = 0
+    subarray_sum = 0
+    min_window_length = nums_length+1
+
+    for right in range(nums_length):
+        subarray_sum += nums[right]
+
+        while subarray_sum >= target:
+            window_length = right - left + 1
+            min_window_length = min(min_window_length, window_length)
+            subarray_sum -= nums[left]
+            left += 1
+
+    if min_window_length > nums_length:
+        return 0
+
+    return min_window_length
+
+
+target = 7
+nums = [2,3,1,2,4,3]
+print(minSubArrayLen(target, nums))
