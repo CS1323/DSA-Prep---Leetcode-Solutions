c7883f5226d2707c67226fd2a0a4b9be25e66700
Cheyenne Sterbick <sterbickc@gmail.com>
2025-08-08 00:09:15 -0700

    feat: add Leetcode 159 solution
    Longest Substring with At Most Two Distinct Characters
    Practice for sliding window with variable conditions.
    Used hashmap (dict) to store frequency of characters in substring.

---
+++ 159_LongestSubstringWithAtMostTwoDistinctCharacters.py
@@ -0,0 +1,31 @@
+def lengthOfLongestSubstring(s) -> int:
+    left = 0
+    char_freq = {}
+    max_window_length = 0
+
+    # sliding window for substring
+    for right in range(len(s)):
+
+        if s[right] in char_freq:
+            char_freq[s[right]] += 1
+        else:
+            char_freq[s[right]] = 1
+
+        # shrink from left if more than 2 distinct characters
+        while len(char_freq) > 2:
+            char_freq[s[left]] -= 1
+
+            if char_freq[s[left]] <= 0:
+                del char_freq[s[left]]
+
+            left += 1
+
+        max_window_length = max(right-left+1, max_window_length)
+
+    return max_window_length
+
+    # Time:  O(n)
+    # Space: O(1)
+
+s = "ccaabbb"
+print(lengthOfLongestSubstring(s))
