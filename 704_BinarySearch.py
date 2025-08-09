8685d012a15f728401466389127600829bcb636a
Cheyenne Sterbick <sterbickc@gmail.com>
2025-08-08 23:59:08 -0700

    feat: add Leetcode 704 solution
    Binary Search
    Practice for binary search.

---
+++ 704_BinarySearch.py
@@ -0,0 +1,22 @@
+def search(nums, target) -> int:
+    left = 0
+    right = len(nums)-1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+    # Time:  O(log n)
+    # Space: O(1)
+
+nums = [-1,0,3,5,9,12]
+target = 9
+print(search(nums, target))
