8e1c42bb81206489dfeb437b418c4077ee767256
Cheyenne Sterbick <sterbickc@gmail.com>
2025-10-10 23:11:45 -0700

    perf: optimize Leetcode 1584 solution
    Min Cost to Connect All Points
    Practice for graphs (weighted, connected). Optimized Prim's algorithm by
    removing the min heap. The graphs in this problem are complete. The heap
    did not reduce asymptotic complexity but instead added unnecessary
    overhead. The new solution followed Prim's invariant by always adding
    the smallest edge connecting a new node, reducing the time and space
    complexities.

--- 1584_MinCostToConnectAllPoints.py
+++ 1584_MinCostToConnectAllPoints.py
@@ -3,30 +3,43 @@ import heapq
 def minCostConnectPoints(points) -> int:
 
     n = len(points)
+    dist = [float("inf")] * n   # min distance
+    visited = [False] * n
+    node, edges = 0, 0
     total_dist = 0
-    visited = set()
-    min_heap = [(0, 0)]     # (dist, point_index)
 
     # Prim's algorithm to create a Minimum Spanning Tree (MST)
-    while len(visited) < n:
-        dist, i = heapq.heappop(min_heap)
+    while edges < n-1:
+        visited[node] = True
+        nextNode = -1
 
-        if i in visited: continue
+        # update min distance for all unvisited nodes
+        for i in range(n):
 
-        total_dist += dist
-        visited.add(i)
+            # skip nodes currently in MST
+            if visited[i]: continue
 
-        # Push edges from this new point to all unvisited points
-        for j in range(n):
+            # compute Manhattan distance
             x1, y1 = points[i]
-            x2, y2 = points[j]
-            new_dist = abs(x1 - x2) + abs(y1 - y2)
-            heapq.heappush(min_heap, (new_dist, j))
+            x2, y2 = points[node]
+            curr_dist = abs(x1 - x2) + abs(y1 - y2)
+
+            # keep smallest edge connecting this node to the MST
+            dist[i] = min(dist[i], curr_dist)
+            
+            # track next node w/smallest connection cost
+            if nextNode == -1 or dist[i] < dist[nextNode]:
+                nextNode = i
+
+        # add new edge
+        total_dist += dist[nextNode]
+        node = nextNode
+        edges += 1
 
     return total_dist
 
-    # Time:  O(n**2 log n)
-    # Space: O(n**2)
+    # Time:  O(n**2)
+    # Space: O(n)
 
 points = [[0,0],[2,2],[3,10],[5,2],[7,0]]
 points = [[3,12],[-2,5],[-4,1]]
