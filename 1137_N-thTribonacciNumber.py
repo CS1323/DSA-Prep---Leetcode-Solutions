4ca92c0da92e45bbadac12099864fe1d34c62e4a
Cheyenne Sterbick <sterbickc@gmail.com>
2025-11-12 16:47:09 -0800

    perf: optimize Leetcode 1137 solution
    N-th Tribonacci Number
    Practice for DP. Decreased space complexity from O(n) to O(1). The
    immediate previous values were stored instead of an n-sized array.

--- 1137_N-thTribonacciNumber.py
+++ 1137_N-thTribonacciNumber.py
@@ -1,16 +1,18 @@
 def tribonacci(n: int) -> int:
 
-    dp = [1] * (n+1)
-    dp[0] = 0   # base case: T(0)=0, T(1)=1, T(2)=1
+    # base cases: T(0)=0, T(1)=1, T(2)=1
+    prev = [0, 1, 1]
 
+    # Bottom-Up DP
     for i in range(3, n+1):
 
-        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        curr = sum(prev)
+        prev[i % 3] = curr
 
-    return dp[n]
+    return prev[n % 3]
 
     # Time:  O(n)
-    # Space: O(n)
+    # Space: O(1)
 
 n = 25
 print(tribonacci(n))
