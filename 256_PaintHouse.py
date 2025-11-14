19105c9a01de2f2a05e1abc55ebbd7934e1bfe28
Cheyenne Sterbick <sterbickc@gmail.com>
2025-11-13 21:51:42 -0800

    perf: optimize Leetcode 256 solution
    Paint House
    Practice for DP. Reduced space complexity from O(n) to O(1) by storing
    only one house's costs for each color at a time.

--- 256_PaintHouse.py
+++ 256_PaintHouse.py
@@ -2,26 +2,27 @@ def minCost(costs) -> int:
 
     n = len(costs)
 
-    # Initialize DP array: dp[i][c] = min cost to paint house i with color c
-    dp = [[float("inf")] * 3 for _ in range(n)]
-    dp[0] = costs[0]    # base case: initialize first house with given costs
+    # Initialize: dp[c] = min cost to paint a house with color c
+    curr = costs[0]
 
     # Bottom-Up DP
     for i in range(1, n):
 
+        prev = curr.copy()
+
         # if curr house is painted red (0), need min cost from blue (1) or green (2)
-        dp[i][0] = costs[i][0] + min(dp[i-1][1], dp[i-1][2])
+        curr[0] = costs[i][0] + min(prev[1], prev[2])
 
         # if curr house is painted blue (1), need min cost from red (0) or green (2)
-        dp[i][1] = costs[i][1] + min(dp[i-1][0], dp[i-1][2])
+        curr[1] = costs[i][1] + min(prev[0], prev[2])
 
         # if curr house is painted green (2), need min cost from red (0) or blue (1)
-        dp[i][2] = costs[i][2] + min(dp[i-1][0], dp[i-1][1])
+        curr[2] = costs[i][2] + min(prev[0], prev[1])
 
-    return min(dp[-1])
+    return min(curr)
 
     # Time:  O(n)
-    # Space: O(n)
+    # Space: O(1)
 
 costs = [[17,2,17],[16,16,5],[14,3,19]]
 # costs = [[7,6,2]]
